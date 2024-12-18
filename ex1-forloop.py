ad4c615405245d9e70a44d5a772dc6ca5086359b
jmckenney3563 <jmckenney0624@gmail.com>
2024-12-17 19:39:27 -0500

    Updated a file ex1 from techteam1 on GitHub

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -1,2 +1,5 @@
 print ("1st modificaation for 2nd commit")
+#---------------------------------------------------------------------
 print ("Added this line of code from jtechdeveloper from git terminal")
+#---------------------------------------------------------------------
+print ("Added 1st line from techteam1 on GitHub")
